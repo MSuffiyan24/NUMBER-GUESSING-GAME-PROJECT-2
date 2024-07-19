b79fd9f23811f450d1153df9a0796ff7db2f957f
MSuffiyan24 <msuffiyanpatni@gmail.com>
2024-07-20 01:06:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const Answer = await inquirer.prompt([{
+        name: 'GuessedNumber',
+        type: 'number',
+        message: "Please Guess a number B/w 1-10",
+    }]);
+if (Answer.GuessedNumber === randomNumber) {
+    console.log("You Guessed the correct number");
+}
+else {
+    console.log("you guessed the wrong number");
+}
